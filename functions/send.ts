fa65619dabe75ac840e87b3152475b33469d4e52
Paul Chavard <github@paul.chavard.net>
2021-05-03 20:45:16 +0200

    fist implementation

--- functions/send.ts
+++ functions/send.ts
@@ -1,10 +1,70 @@
 import { Handler } from '@netlify/functions';
+import { createTransport } from 'nodemailer';
+
+const AUTH_TOKEN = process.env['AUTH_TOKEN'];
 
 const handler: Handler = async (event, context) => {
+  if (event.httpMethod == 'POST') {
+    const token = (event.headers['authorization'] ?? '')
+      .replace('Bearer', '')
+      .trim();
+    if (token != AUTH_TOKEN) {
+      return {
+        statusCode: 403,
+        body: 'Forbidden',
+      };
+    }
+    const body = JSON.parse(event.body) as Body;
+    const transport = getTransport(body.smtp);
+    const ok = await transport.verify();
+
+    if (ok) {
+      for (const message of body.messages) {
+        await transport.sendMail(message);
+      }
+      return {
+        statusCode: 200,
+        body: JSON.stringify({ ok }),
+      };
+    } else {
+      return {
+        statusCode: 400,
+        body: 'Invalid SMTP config',
+      };
+    }
+  }
   return {
-    statusCode: 200,
-    body: JSON.stringify({ message: 'Hello World' }),
+    statusCode: 404,
+    body: 'Not Found',
   };
 };
+
+type Transport = ReturnType<typeof createTransport>;
+type Message = Parameters<Transport['sendMail']>[0];
+
+type SMTPConfig = {
+  host: string;
+  port: number;
+  auth: {
+    user: string;
+    pass: string;
+  };
+};
+
+type Body = {
+  smtp: SMTPConfig;
+  messages: Message[];
+};
+
+function getTransport(smtp: SMTPConfig) {
+  return createTransport({
+    ...smtp,
+    secure: true,
+    tls: {
+      rejectUnauthorized: true,
+      minVersion: 'TLSv1.2',
+    },
+  });
+}
 
 export { handler };
